2415babde9d984c498f16dff373ffa6c94d99293
christianwalpen <christianwalpen@opacc.ch>
2018-04-11 22:28:55 +0200

    Added pack.ps1 for nuget packaging

---
+++ pack.ps1
@@ -0,0 +1,13 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition)
+
+$version = [System.Reflection.Assembly]::LoadFile("$root\src\MeasureMap\bin\Release\MeasureMap.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}-RC0{3}" -f ($version.Major, $version.Minor, $version.Build, $version.Revision)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\src\MeasureMap.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\src\MeasureMap.compiled.nuspec
+
+& $root\build\NuGet.exe pack $root\src\MeasureMap.compiled.nuspec
