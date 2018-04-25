175c216c2b4e44a0af43d0634dfa7006dc42c338
christianwalpen <christianwalpen@opacc.ch>
2018-04-25 22:24:03 +0200

    Update deployment

--- pack.ps1
+++ pack.ps1
@@ -1,5 +1,7 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition)
 
+<#
+# Create NuGet package for RC
 $version = [System.Reflection.Assembly]::LoadFile("$root\src\MeasureMap\bin\Release\MeasureMap.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}-RC0{3}" -f ($version.Major, $version.Minor, $version.Build, $version.Revision)
 
@@ -11,9 +13,9 @@ $content = $content -replace '\$version\$',$versionStr
 $content | Out-File $root\src\MeasureMap.compiled.nuspec
 
 & $root\build\NuGet.exe pack $root\src\MeasureMap.compiled.nuspec
+#>
 
-
-
+# Create NuGet package for Release
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
