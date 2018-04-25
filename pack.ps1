0ae55d8b1db9ed736cc0a83da8f7a113021fef6d
christianwalpen <christianwalpen@opacc.ch>
2018-04-25 22:31:16 +0200

    Fix Powershell for building nupkg

--- pack.ps1
+++ pack.ps1
@@ -1,8 +1,8 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition)
+$version = [System.Reflection.Assembly]::LoadFile("$root\src\MeasureMap\bin\Release\MeasureMap.dll").GetName().Version
 
 <#
 # Create NuGet package for RC
-$version = [System.Reflection.Assembly]::LoadFile("$root\src\MeasureMap\bin\Release\MeasureMap.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}-RC0{3}" -f ($version.Major, $version.Minor, $version.Build, $version.Revision)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
