6a849a5688725168c9d4451f017c207dcc15a2b6
Christian Walpen <christianwalpen@opacc.ch>
2022-02-23 07:04:01 +0100

    Ignore testfile on sonarcloud

--- src/Tests/MeasureMap.UnitTest/EnumerationTests.cs
+++ src/Tests/MeasureMap.UnitTest/EnumerationTests.cs
@@ -6,6 +6,7 @@ using NUnit.Framework;
 
 namespace MeasureMap.UnitTest
 {
+#pragma warning disable L123
     public class EnumerationTests
     {
         [Test]
@@ -212,4 +213,5 @@ namespace MeasureMap.UnitTest
             }
         }
     }
+#pragma warning restore L123
 }
