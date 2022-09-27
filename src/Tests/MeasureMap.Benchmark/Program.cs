658e505f82ce828127c60e909cad3e84cdefd7bc
Christian Walpen <christianwalpen@opacc.ch>
2022-09-27 08:15:23 +0200

    Added Benchmark console app

---
+++ src/Tests/MeasureMap.Benchmark/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MeasureMap;
+using MeasureMap.Tracers;
+
+var benchmark = new BenchmarkRunner();
+benchmark.SetIterations(10);
+benchmark.Task("Setup 1 Thread", () =>
+{
+    ProfilerSession.StartSession()
+        .SetThreads(1)
+        .Task(c2 =>
+        {
+            // do nothing
+        })
+        .RunSession();
+}).SetThreads(10);
+
+benchmark.Task("Setup 10 Threads", () =>
+{
+    ProfilerSession.StartSession()
+        .SetThreads(1)
+        .Task(c2 =>
+        {
+            // do nothing
+        })
+        .RunSession();
+}).SetThreads(10);
+
+
+var options = new TraceOptions();
+options.Metrics = new MeasureMap.Benchmark.TraceMetrics();
+
+benchmark.RunSessions()
+    .Trace(options);
+
+
---
+++ src/Tests/MeasureMap.Benchmark/TraceMetrics.cs
@@ -0,0 +1,21 @@
+﻿
+using MeasureMap.Tracers.Metrics;
+
+namespace MeasureMap.Benchmark
+{
+    /// <summary>
+    /// Defaultmetrics for <see cref="IBenchmarkResult"/>
+    /// </summary>
+    public class TraceMetrics : Tracers.TraceMetrics
+    {
+        public TraceMetrics()
+        {
+            Add(ProfilerMetric.AverageTime);
+            Add(ProfilerMetric.AverageMilliseconds);
+            Add(ProfilerMetric.Fastest);
+            Add(ProfilerMetric.Slowest);
+            Add(ProfilerMetric.Threads);
+            Add(ProfilerMetric.Throughput);
+        }
+    }
+}
