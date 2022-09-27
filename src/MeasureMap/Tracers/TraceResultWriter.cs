775c535ec6952fd1d8dd94a6871fa85cd3162b4e
Christian Walpen <christianwalpen@opacc.ch>
2022-09-27 12:36:54 +0200

    Differntiate between Trace and Console

--- src/MeasureMap/Tracers/TraceResultWriter.cs
+++ src/MeasureMap/Tracers/TraceResultWriter.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace MeasureMap.Tracers
 {
@@ -7,13 +9,16 @@ namespace MeasureMap.Tracers
     /// </summary>
     public class TraceResultWriter : IResultWriter
     {
+        private static Action<string> _write = Environment.UserInteractive ? s => Console.Write(s) : s => Trace.Write(s);
+        private static Action<string> _writeLine = Environment.UserInteractive ? s => Console.WriteLine(s) : s => Trace.WriteLine(s);
+
         /// <summary>
         /// Write a value to the <see cref="Trace"/>
         /// </summary>
         /// <param name="value"></param>
         public void Write(string value)
         {
-            Trace.Write(value);
+            _write(value);
         }
 
         /// <summary>
@@ -22,7 +27,7 @@ namespace MeasureMap.Tracers
         /// <param name="value"></param>
         public void WriteLine(string value)
         {
-            Trace.WriteLine(value);
+            _writeLine(value);
         }
     }
 }
