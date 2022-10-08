43e7f843cf5c131a1689fb12350cc571447a9f7e
Christian Walpen <christianwalpen@opacc.ch>
2022-10-08 23:33:20 +0200

    Add samples

---
+++ src/Tests/MeasureMap.Benchmark/WorkerThreadListBenchmarks.cs
@@ -0,0 +1,20 @@
+﻿using MeasureMap.Threading;
+
+namespace MeasureMap.Benchmark
+{
+    public class WorkerThreadListBenchmarks
+    {
+        public void RunBenchmarks()
+        {
+            var threads = new WorkerThreadList();
+
+            var benchmark = new BenchmarkRunner();
+            benchmark.SetIterations(10);
+
+            benchmark.Task("Setup 1 Thread", ctx => threads.StartNew(ctx.Get<int>(ContextKeys.Iteration), () => new Result()));
+
+            benchmark.RunSessions()
+                .Trace(new MeasureMap.Benchmark.TraceMetrics());
+        }
+    }
+}
