0187fb0a63525c4b7ab74a9a3d679e27e4939fa0
Christian Walpen <christianwalpen@opacc.ch>
2022-10-08 23:44:05 +0200

    Smples

--- src/Tests/MeasureMap.Benchmark/Program.cs
+++ src/Tests/MeasureMap.Benchmark/Program.cs
@@ -1,5 +1,17 @@
-﻿
-using MeasureMap.Benchmark;
+﻿using MeasureMap.Benchmark;
 
-new StartSessionBenchmarks().RunBenchmarks();
+var bm = "BenchmarkSample";
+
+switch (bm)
+{
+    case nameof(BenchmarkSample):
+        new BenchmarkSample().RunBenchmarks();
+        break;
+    case nameof(StartSessionBenchmarks):
+        new StartSessionBenchmarks().RunBenchmarks();
+        break;
+    case nameof(WorkerThreadListBenchmarks):
+        new WorkerThreadListBenchmarks().RunBenchmarks();
+        break;
+}
 
